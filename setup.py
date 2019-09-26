067b0d9e8992b2f376c11e8ede55d6e10f576b56
HAMANO Tsukasa <hamano@osstech.co.jp>
2019-09-26 16:35:19 +0900

    release 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='Flask-Mail-SendGrid',
-    version='0.3',
+    version='0.4',
     description="Flask extension for sendgrid. It has same interface with Flask-Mail.",
     long_description=long_description,
     url="http://github.com/hamano/flask-mail-sendgrid",
